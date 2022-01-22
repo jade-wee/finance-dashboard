f112aadbcf86b8ec3d92531bc1f1531a832d6c5d
Jade Wee <jadeweeee@gmail.com>
2022-01-23 03:43:49 +0800

    Update nav bar

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders coming soon text', () => {
+test("renders app", () => {
   render(<App />);
-  const h1Element = screen.getByText(/Hello/i);
-  expect(h1Element).toBeInTheDocument();
 });
